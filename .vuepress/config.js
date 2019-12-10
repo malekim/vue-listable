855f3fb3177e712a4b151e700df0d621db568205
Łukasz Małek <dev@malek.im>
2019-12-10 14:29:47 +0100

    VuePress

---
+++ .vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  base: '/vue-listable/',
+  title: 'vue-listable',
+  dest: 'docs',
+  description: 'An advanced data table plugin for vue.js and nuxt.js',
+  themeConfig: {
+    repo: 'malekim/vue-listable',
+    sidebar: {
+      '/': [
+        {
+          title: 'Introduction',
+          collapsable: false,
+          children: [
+            '',
+          ]
+        }
+      ],
+    },
+  }
+}
