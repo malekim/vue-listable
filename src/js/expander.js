c8cdef43bcda7df2064d8b6f2921583eb029e9fe
Łukasz Małek <dev@malek.im>
2019-12-10 09:57:40 +0100

    Expander component

---
+++ src/js/expander.js
@@ -0,0 +1,44 @@
+const Expander = {
+  render: function (createElement) {
+    if (this.expanderSlot) {
+      return createElement(
+        "tr",
+        {
+          class: {
+            "listable-tr": true,
+            "listable-tr-expander": true
+          }
+        },
+        [
+          createElement(
+            "td",
+            {
+              class: [
+                "listable-td"
+              ],
+              attrs: {
+                colspan: this.colspan
+              }
+            },
+            this.expanderSlot(this.row)
+          )
+        ]
+      );
+    }
+  },
+  props: {
+    colspan: {
+      type: Number,
+      default: 0
+    },
+    row: {
+      type: Object,
+      default: () => {}
+    },
+    expanderSlot: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+export default Expander;
