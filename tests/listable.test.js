009f6f7396490234445ce37106f187939986d0b1
Łukasz Małek <dev@malek.im>
2019-12-13 00:59:35 +0100

    New test cases

--- tests/listable.test.js
+++ tests/listable.test.js
@@ -11,6 +11,67 @@ describe('Component', () => {
   test('Component render', () => {
     const wrapper = shallowMount(Listable)
 
-    expect(wrapper.find('.listable-td').exists()).toBe(true)
+    expect(wrapper.find('.listable').exists()).toBe(true)
+  })
+
+  test('Checkbox with empty data', () => {
+    const wrapper = shallowMount(Listable)
+
+    expect(wrapper.find('.listable-checkbox').exists()).toBe(false)
+  })
+
+  test('Checkbox', () => {
+    const wrapper = shallowMount(Listable, {
+      propsData: { 
+        checkbox: true,
+        headings: [
+          {
+            display: "ID",
+            column: "id",
+          },
+          {
+            display: "Name",
+            column: "name",
+          }
+        ],
+        data: [
+          {
+            id: 1,
+            name: "Test"
+          }
+        ]
+      }
+    })
+
+    expect(wrapper.find('.listable-checkbox').exists()).toBe(true)
+  })
+
+  test('Expected error', () => {
+    const consoleSpy = jest.spyOn(global.console, 'error');
+    const wrapper = shallowMount(Listable, {
+      propsData: { 
+        headings: [
+          {
+            display: "ID",
+            column: "id",
+          },
+          {
+            display: "Name",
+            column: "name",
+          }
+        ],
+        data: [
+          {
+            id: 1
+          }
+        ]
+      }
+    })
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      'Data does not contain column name'
+    );
+    
+    consoleSpy.mockReset();
   })
 })
