b07e031bcf1a9c137553645a04cee3a3237080ab
Łukasz Małek <dev@malek.im>
2019-12-12 16:04:19 +0100

    Basic component test

--- tests/listable.test.js
+++ tests/listable.test.js
@@ -1,9 +1,16 @@
 import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Listable from '../src/js/listable';
 
 describe('Component', () => {
   test('Is a Vue instance', () => {
     const wrapper = mount(Listable)
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('Component render', () => {
+    const wrapper = shallowMount(Listable)
+
+    expect(wrapper.find('.listable-td').exists()).toBe(true)
   })
 })
