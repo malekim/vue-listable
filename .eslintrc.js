6f8cbf14ed1041348490c9ad0a8017f1918d2690
Łukasz Małek <dev@malek.im>
2019-12-17 15:20:58 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   parserOptions: {
     "parser": "babel-eslint",
-    "ecmaVersion": 6,
+    "ecmaVersion": 2017,
     "sourceType": "module"
   },
   // add your custom rules here
